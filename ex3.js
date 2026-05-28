84c6f5853b1fdefe73f95f3311db4f5ab75d201e
NvDuc-JV260327 <Nvduc9620@gmail.com>
2026-05-28 21:11:47 +0700

    Update ex3.js

--- ex3.js
+++ ex3.js
@@ -1,22 +1,12 @@
-let modal = document.getElementById("modal");
-
-let openBtn = document.getElementById("openBtn");
-
-let closeBtn = document.querySelector(".close");
-
-// Mở modal
-openBtn.onclick = function () {
-    modal.style.display = "block";
-};
-
-// Đóng khi bấm X
-closeBtn.onclick = function () {
-    modal.style.display = "none";
-};
-
-// Đóng khi bấm ra ngoài
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
+let open = document.querySelector('.openModal');
+let close = document.querySelector('.close');
+let container = document.querySelector('.container');
+let modal = document.querySelector('.modal');
+open.onclick = function() {
+    modal.style.visibility = 'visible';
+    container.style.backgroundColor = '#8a9ea8';
+}
+close.onclick = function() {
+    modal.style.visibility = 'hidden';
+    container.style.backgroundColor = 'white';
+}
